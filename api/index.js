77ee7197a73276f6ffe91cc0c4046c87dd05cfce
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-09 19:07:37 +0530

    1:51:09

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const app = express();
 const port = 8000;
@@ -71,5 +72,136 @@ app.get("/getEmployees", async (req, res) => {
     res.status(200).json(employees);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch employees" });
+  }
+});
+
+app.post("/attendance", async (req, res) => {
+  try {
+    const { employeeId, date, time, status } = req.body;
+
+    const existingAttendance = await Attendance.findOne({
+      employeeId,
+      date,
+    });
+
+    if (existingAttendance) {
+      existingAttendance.status.push(status);
+      await existingAttendance.save();
+      return res.status(200).json(existingAttendance);
+    } else {
+      const newAttendance = new Attendance({
+        employeeId,
+        employeeName,
+        date,
+        status,
+      });
+      await newAttendance.save();
+      return res.status(200).json(newAttendance);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to add attendance" });
+  }
+});
+
+app.get("/attendance", async (req, res) => {
+  try {
+    const { date } = req.query;
+
+    const attendanceDate = await Attendance.find({ date: date });
+
+    res.status(200).json(attendanceDate);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch attendance" });
+  }
+});
+
+app.get("/attendance-report-all-employees", async (req, res) => {
+  try {
+    const { month, year } = req.query;
+
+    console.log("Query parameters:", month, year);
+    // Calculate the start and end dates for the selected month and year
+    const startDate = moment(`${year}-${month}-01`, "YYYY-MM-DD")
+      .startOf("month")
+      .toDate();
+    const endDate = moment(startDate).endOf("month").toDate();
+
+    // Aggregate attendance data for all employees and date range
+    const report = await Attendance.aggregate([
+      {
+        $match: {
+          $expr: {
+            $and: [
+              {
+                $eq: [
+                  { $month: { $dateFromString: { dateString: "$date" } } },
+                  parseInt(req.query.month),
+                ],
+              },
+              {
+                $eq: [
+                  { $year: { $dateFromString: { dateString: "$date" } } },
+                  parseInt(req.query.year),
+                ],
+              },
+            ],
+          },
+        },
+      },
+
+      {
+        $group: {
+          _id: "$employeeId",
+          present: {
+            $sum: {
+              $cond: { if: { $eq: ["$status", "present"] }, then: 1, else: 0 },
+            },
+          },
+          absent: {
+            $sum: {
+              $cond: { if: { $eq: ["$status", "absent"] }, then: 1, else: 0 },
+            },
+          },
+          halfday: {
+            $sum: {
+              $cond: { if: { $eq: ["$status", "halfday"] }, then: 1, else: 0 },
+            },
+          },
+          holiday: {
+            $sum: {
+              $cond: { if: { $eq: ["$status", "holiday"] }, then: 1, else: 0 },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "employees", // Name of the employee collection
+          localField: "_id",
+          foreignField: "employeeId",
+          as: "employeeDetails",
+        },
+      },
+      {
+        $unwind: "$employeeDetails", // Unwind the employeeDetails array
+      },
+      {
+        $project: {
+          _id: 1,
+          present: 1,
+          absent: 1,
+          halfday: 1,
+          name: "$employeeDetails.employeeName",
+          designation:"$employeeDetails.designation",
+          salary: "$employeeDetails.salary",
+          employeeId: "$employeeDetails.employeeId",
+        },
+      },
+    ]);
+
+    res.status(200).json({ report });
+  } catch (error) {
+    console.error("Error generating attendance report:", error);
+    res.status(500).json({ message: "Error generating the report" });
   }
 });
