9eb6c33edcbf16c2d1ddd95b71928a27987969e3
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-09 15:54:45 +0530

    38:51

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -1,14 +1,151 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Feather, Entypo, Ionicons, Octicons } from "@expo/vector-icons";
 
 const index = () => {
   return (
     <ScrollView>
-      <Text>index</Text>
+      <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ flex: 1 }}>
+        <View style={{ padding: 12 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Feather name="bar-chart" size={24} color="black" />
+            <Text style={{ fontSize: 16, fontWeight: "600" }}>
+              Employee Managament System
+            </Text>
+            <Entypo name="lock" size={24} color="black" />
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 20,
+            }}
+          >
+            <Pressable
+              onPress={() => router.push("/(home)/employees")}
+              style={{
+                backgroundColor: "#D3CCE3",
+                padding: 12,
+                borderRadius: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-people-sharp" size={24} color="black" />
+              </View>
+              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+                Employee List
+              </Text>
+            </Pressable>
+            <Pressable
+              onPress={() => router.push("/(home)/markattendance")}
+              style={{
+                backgroundColor: "#D3CCE3",
+                padding: 12,
+                borderRadius: 6,
+                alignItems: "center",
+                justifyContent: "center",
+                flex: 1,
+              }}
+            >
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-people-sharp" size={24} color="black" />
+              </View>
+              <Text style={{ marginTop: 7, fontWeight: "600" }}>
+                Mark Attendance
+              </Text>
+            </Pressable>
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              backgroundColor: "white",
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+              borderRadius: 7,
+            }}
+          >
+            <Pressable
+              style={{
+                backgroundColor: "#BE93C5",
+                borderRadius: 6,
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+                marginVertical: 10,
+              }}
+            >
+              <View
+                style={{
+                  padding: 7,
+                  width: 45,
+                  height: 45,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="newspaper-outline" size={24} color="black" />
+              </View>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontSize: 16,
+                  fontWeight: "600",
+                  flex: 1,
+                }}
+              >
+                Attendance Report
+              </Text>
+              <View
+                style={{
+                  width: 35,
+                  height: 35,
+                  borderRadius: 7,
+                  backgroundColor: "white",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Entypo name="chevron-right" size={24} color="black" />
+              </View>
+            </Pressable>
+          </View>
+        </View>
+      </LinearGradient>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default index
+export default index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
