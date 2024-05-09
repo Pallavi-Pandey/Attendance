9333b58fe98be94e31705d2de1ef1744264ecdb9
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-09 16:19:43 +0530

    46:51
    55:41

--- api/index.js
+++ api/index.js
@@ -23,5 +23,53 @@ mongoose
   });
 
 app.listen(port, () => {
-    console.log("Server is running on port: " + port);
+  console.log("Server is running on port: " + port);
+});
+
+const Employee = require("./models/employee");
+const Attendance = require("./models/attendance");
+
+//endpoint to register a employee
+app.post("/addEmployee", async (req, res) => {
+  try {
+    const {
+      employeeId,
+      employeeName,
+      designation,
+      joiningDate,
+      dateOfBirth,
+      salary,
+      activeEmployee,
+      phoneNumber,
+      address,
+    } = req.body;
+
+    //create a new employee
+    const newEmployee = new Employee({
+      employeeId,
+      employeeName,
+      designation,
+      joiningDate,
+      dateOfBirth,
+      salary,
+      activeEmployee,
+      phoneNumber,
+      address,
+    });
+    await newEmployee.save();
+
+    res.status(200).json({ message: "Employee added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to add employee" });
+  }
+});
+
+//endpoint to fetch all employees
+app.get("/getEmployees", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch employees" });
+  }
 });
