e0dd65f113971ff8e2975ac187c6eb4d9648c198
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-06-28 19:40:43 +0530

    last

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
 
-const index = () => {
+const index = () => { 
   return (
    <Redirect href="/(home)"/>
   )
